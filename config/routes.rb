31f6e522dd54418f2a4a0dc08b61cd7bcf32d10f
Hayri <hayri@hoshilab.com>
2014-10-09 12:47:16 +0200

    implementing the user and session controllers "

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  resources :users
+  resources :sessions
+
+
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
